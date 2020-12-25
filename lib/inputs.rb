c7db74a147fcba833c5fa07f0ca13643592623f8
Rafael Dias <rafa197795@gmail.com>
2020-12-25 20:32:59 -0300

    refactor(day01): add functions for test data
    Signed-off-by: Rafael Dias <rafa197795@gmail.com>

--- lib/inputs.rb
+++ lib/inputs.rb
@@ -3,14 +3,14 @@ module Inputs
     if real_data
       raw_inputs = File.read("data/day#{day}.txt")
     else
-      raw_inputs = """1721
-979
-366
-299
-675
-1456"""
+      raw_inputs = self.method("day#{day}").call
     end
 
     block.call(raw_inputs)
   end
+
+  private
+    def self.day1
+    "1721\n979\n366\n299\n675\n1456"
+    end
 end
